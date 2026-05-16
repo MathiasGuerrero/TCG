df2f4e4a5db2030dead9c8158c0af4148d991f6c
MathiasGuerrero <math.guerreroc@duocuc.cl>
2026-05-16 18:50:37 -0400

    Add files via upload

---
+++ Cartas/FireTCG_Store/src/main/resources/db/migration/V1__crear_tabla_carta.sql
@@ -0,0 +1,6 @@
+   CREATE table carta (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  tcg VARCHAR(20),
+  nombre VARCHAR(30),
+  anio INT(4)
+  );
---
+++ Cartas/FireTCG_Store/src/test/java/com/example/FireTCG_Store/FireTcgStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FireTCG_Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireTcgStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
