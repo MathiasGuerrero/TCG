a00ec218f1bf7cdebd869c274bad2cb2feb7847b
MathiasGuerrero <math.guerreroc@duocuc.cl>
2026-05-16 02:43:22 -0400

    Add files via upload

---
+++ usuario/usuario/usuario/src/main/resources/db/migration/V2__crear_usuarios.sql
@@ -0,0 +1,11 @@
+INSERT INTO usuario (username, correo) VALUES
+('carlos_mg', 'carlos.mg@gmail.com'),
+('maria_lopez', 'maria.lopez@hotmail.com'),
+('juan_perez', 'juan.perez@gmail.com'),
+('ana_torres', 'ana.torres@yahoo.com'),
+('luis_ramos', 'luis.ramos@gmail.com'),
+('sofia_vera', 'sofia.vera@hotmail.com'),
+('pedro_diaz', 'pedro.diaz@outlook.com'),
+('laura_rios', 'laura.rios@gmail.com'),
+('diego_mora', 'diego.mora@yahoo.com'),
+('valeria_ng', 'valeria.ng@gmail.com');
